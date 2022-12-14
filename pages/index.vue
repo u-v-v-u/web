42eb2c96d5a6187e98621dd9e4be537a2a09e8b7
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-12-14 20:18:37 +1100

    Add navbar & Update center text

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <script setup lang='ts'>
 import '@/styles/main.css'
 
+const { $umami } = useNuxtApp()
+const trackButtonClick = (sort: string): void => $umami(`${sort} | Clicked`)
+
 let uwuText = useState('uwuText', () => '')
 
 try {
@@ -8,17 +11,31 @@ try {
 
   if (!res) throw null
 
-  uwuText.value = res
+  uwuText.value = `${res}\n\t\tRust & TypeScript Development`
 } catch {
   uwuText.value = ''
 }
-
 </script>
 
 <template>
-  <div class='prose prose-zinc lg:prose-xl'>
-    <div class='flex flex-col justify-center items-center h-screen w-screen'>
-      <pre id='fade-in' class='text-xl min-h-60 min-w-60' style='opacity: 0; transition: opacity 5s ease-in;'>{{ uwuText }}</pre>
+  <div class='prose prose-zinc'>
+    <div class='w-screen h-screen flex flex-col '>
+      <!--  Nav, Buttons-->
+      <div class='flex justify-start items-start'>
+        <nav class='text-[1.3rem] flex flex-col justify-start pt-3 pl-4 m-0'>
+          <div class='flex items-center'>
+            <a @click="trackButtonClick('mailto')" href='mailto:uwu@uvvu.pw' class='nav-button text-white'>Email</a>
+            <a @click="trackButtonClick('mailto')" href='https://github.com/u-v-v-u'
+              class='nav-button text-white'>Github</a>
+          </div>
+        </nav>
+      </div>
+
+      <!--  Center, UwU ascii art-->
+      <div id='fade-in' class='flex flex-col justify-center items-center h-screen'
+        style='opacity: 0; transition: opacity 5s ease;'>
+        <pre class='text-xl min-h-60 min-w-60 p-0 m-0'>{{ uwuText }}</pre>
+      </div>
     </div>
   </div>
 </template>
