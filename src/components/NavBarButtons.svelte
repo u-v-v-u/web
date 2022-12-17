d146647764231e443b08962989e9987e1e5984d8
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-12-17 16:52:29 +1100

    fix: Fix div

--- src/components/NavBarButtons.svelte
+++ src/components/NavBarButtons.svelte
@@ -9,18 +9,20 @@
   };
 </script>
 
-<a on:click={() => trackButton('Mail To')} href="mailto:uwu@uvvu.pw" class="nav-button">
-  Email <Icon icon="eva:email-outline" />
-</a>
-<a on:click={() => trackButton('Github')} href="https://github.com/u-v-v-u" class="nav-button">
-  Github <Icon icon="mdi:github-box" />
-</a>
-{#if $page.url.pathname != '/projects'}
-  <a on:click={() => trackButton('Projects')} href="/projects" class="nav-button"
-    >Projects <Icon icon="mdi:microsoft-visual-studio-code" /></a
-  >
-{:else}
-  <a on:click={() => trackButton('Home')} href="/" class="nav-button"
-    >Home <Icon icon="mdi:home" /></a
-  >
-{/if}
+<div class="flex">
+  <a on:click={() => trackButton('Mail To')} href="mailto:uwu@uvvu.pw" class="nav-button">
+    Email <Icon icon="eva:email-outline" />
+  </a>
+  <a on:click={() => trackButton('Github')} href="https://github.com/u-v-v-u" class="nav-button">
+    Github <Icon icon="mdi:github-box" />
+  </a>
+  {#if $page.url.pathname != '/projects'}
+    <a on:click={() => trackButton('Projects')} href="/projects" class="nav-button"
+      >Projects <Icon icon="mdi:microsoft-visual-studio-code" /></a
+    >
+  {:else}
+    <a on:click={() => trackButton('Home')} href="/" class="nav-button"
+      >Home <Icon icon="mdi:home" /></a
+    >
+  {/if}
+</div>
